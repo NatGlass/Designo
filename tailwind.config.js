3232117c399fde789dec0fbe40c851fbf0f8e5e8
Nathan G <Nathanglass26@gmail.com>
2022-01-19 11:41:32 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+        peach: "rgb(231, 129, 107)",
+        peachLight: "rgb(255, 173, 155)",
+        black: "rgb(29, 28, 30)",
+        greyDark: "rgb(51, 49, 54)",
+        greyLight: "rgb(241, 243, 245)",
+    },
+    fontFamily: {
+      jost: ["jost", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
