8c242fdb782c57feab4dbb4e67fafda61103eb51
Nathan G <Nathanglass26@gmail.com>
2022-01-19 22:41:16 +0000

    constant styles

---
+++ src/ConstStyles.ts
@@ -0,0 +1,8 @@
+
+export const commonsHero = "relative w-full mx-auto bg-peach overflow-hidden md:w-[90%] md:rounded-[15px] lg:w-[95%] max:w-full"
+
+export const commonsH1 = "absolute w-[90%] max-w-[327px] text-[white] text-[32px] text-center font-medium leading-[48px] lg:translate-x-0 z-10"
+
+export const commonsHeroText = "absolute left-2/4 -translate-x-2/4 text-center w-[90%] max-w-[327px] text-[white] text-[16px] leading-[25px] z-10 lg:translate-x-0"
+
+export const commonsButton = "absolute left-2/4 -translate-x-2/4 w-[152px] h-[56px] bg-[white] rounded-[8px] uppercase leading-[1px] z-10 hover:bg-peachLight hover:text-[white] transition-colors duration-500 hover:transition-colors hover:duration-500 lg:translate-x-0"
