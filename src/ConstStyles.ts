9ac4742ba61a8e37cdf5312fc75c6984c4579a6e
Nathan G <Nathanglass26@gmail.com>
2022-01-20 17:03:50 +0000

    services section on home

--- src/ConstStyles.ts
+++ src/ConstStyles.ts
@@ -1,8 +1,25 @@
+// hero //
 
 export const commonsHero = "relative w-full mx-auto bg-peach overflow-hidden md:w-[90%] md:rounded-[15px] lg:w-[95%] max:w-full"
 
 export const commonsH1 = "absolute w-[90%] max-w-[327px] text-[white] text-[32px] text-center font-medium leading-[48px] lg:translate-x-0 z-10"
 
-export const commonsHeroText = "absolute left-2/4 -translate-x-2/4 text-center w-[90%] max-w-[327px] text-[white] text-[16px] leading-[25px] z-10 lg:translate-x-0"
+export const commonsHeroText = "absolute left-2/4 -translate-x-2/4 text-center w-[90%] max-w-[327px] text-[white] text-[16px] leading-[25px] lg:translate-x-0 z-10"
 
 export const commonsButton = "absolute left-2/4 -translate-x-2/4 w-[152px] h-[56px] bg-[white] rounded-[8px] uppercase leading-[1px] z-10 hover:bg-peachLight hover:text-[white] transition-colors duration-500 hover:transition-colors hover:duration-500 lg:translate-x-0"
+
+// services //
+
+export const commonsServicesContainer = "flex flex-col justify-between w-[90%] h-[800px] mx-auto my-[60px] md:max-w-[700px] md:h-[650px] md:my-[120px] lg:w-[95%] lg:max-w-full lg:h-[640px] lg:grid lg:grid-cols-2 lg:my-[160px] lg:gap-[20px] "
+
+export const commonsServicesItem = "relative flex justify-center items-center w-full h-[250px] uppercase rounded-[15px] cursor-pointer bg-no-repeat bg-cover md:h-[200px] lg:h-[308px] after:content-[''] after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-[black] after:opacity-50 hover:after:bg-peach after:transition-colors after:duration-500 after:ease-in-out"
+
+export const commonsServicesItemTitle = "relative text-[white] text-[28px] tracking-[2px] text-center z-10 md:text-[40px]"
+
+export const commonsServicesItemText = "relative text-[white] text-[15px] tracking-[5px] text-center z-10 pt-[6px]"
+
+export const commonsServicesWeb = "bg-web-small lg:bg-web-large"
+
+export const commonsServicesApp = "bg-app"
+
+export const commonsServicesGraphic = "bg-graphic"
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { commonsHero, commonsH1, commonsHeroText, commonsButton } from '../ConstStyles';
+import { commonsHero, commonsH1, commonsHeroText, commonsButton, commonsServicesContainer, commonsServicesItem, commonsServicesItemTitle, commonsServicesItemText, commonsServicesWeb, commonsServicesApp, commonsServicesGraphic } from '../ConstStyles';
 import HeroImage from '../assets/home/desktop/image-hero-phone.png'
 
-const Hero = () => {
-    return (
+const Hero = () => (
         <div className={`${commonsHero} h-screen max-h-[843px] lg:max-h-[640px]`}>
             <h1 className={`${commonsH1} top-[80px] left-2/4 -translate-x-2/4
             md:top-[60px] md:max-w-[573px] md:text-[48px]
@@ -20,11 +19,35 @@ const Hero = () => {
                 learn more
             </button>
             <div className="absolute w-[640px] h-[640px] oval rounded-full left-0 md:top-[101px] md:left-1/4 lg:top-0 lg:left-[43%]" />
-
-            <img src={HeroImage} className="absolute top-[400px] left-2/4 -translate-x-2/4 w-[550px] h-[700px] md:top-[350px] md:w-[550px] md:h-[750px] lg:top-[80px] lg:left-3/4 " />
+            <img src={HeroImage} className="absolute top-[400px] left-2/4 -translate-x-2/4 w-[550px] h-[700px] 
+            md:top-[350px] md:w-[550px] md:h-[750px] 
+            lg:top-0 lg:left-3/4 lg:w-[580px] lg:h-[860px] " />
         </div>
-    );
-};
+);
+
+const Services = () => (
+    
+    <div className={`${commonsServicesContainer}`}>
+        <div className={`${commonsServicesItem} ${commonsServicesWeb} row-span-2 lg:h-full`}>
+            <div>
+                <h2 className={`${commonsServicesItemTitle}`}>web design</h2>
+                <p className={`${commonsServicesItemText}`}>view projects</p>
+            </div>
+        </div>
+        <div className={`${commonsServicesItem} ${commonsServicesApp}`}>
+            <div>
+                <h2 className={`${commonsServicesItemTitle}`}>app design</h2>
+                <p className={`${commonsServicesItemText}`}>view projects</p>
+            </div>
+        </div>
+        <div className={`${commonsServicesItem} ${commonsServicesGraphic}`}>
+            <div>
+                <h2 className={`${commonsServicesItemTitle}`}>graphic design</h2>
+                <p className={`${commonsServicesItemText}`}>view projects</p>
+            </div>
+        </div>
+    </div>
+);
 
 
 
@@ -32,7 +55,7 @@ const Home:React.FC = () => {
     return (
         <>
             <Hero />
-
+            <Services />
         </>
   );
 };
