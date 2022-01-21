a4cba14150e1c0e6896687b9f364dec1f1c56cdb
Nathan G <Nathanglass26@gmail.com>
2022-01-21 17:17:33 +0000

    fixed pages not loading with closing nav on click

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,13 +8,10 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineClose } from 'react-icons/ai';
 
 const Container = ({ children }: { children: React.ReactNode }) => (
-    <nav className="
-        flex w-full z-[100] justify-between items-center
-        h-[96px] px-[24px]
+    <nav className="flex w-full z-[100] justify-between items-center h-[96px] px-[24px]
         md:h-[27px] md:px-[40px] md:my-[64px]
         lg:px-[15px]
-        max:px-0
-        ">
+        max:px-0">
         {children}
     </nav>
 );
@@ -25,26 +22,23 @@ const Logo = () => (
     </Link>
 );
 
-const Links = ({ open }: { open: boolean }, { setOpen }: { setOpen: Function }) => {
-    function CloseNav() {
-        setOpen(!open)
-    }
+const Links = ({ setOpen }: { setOpen: Function }) => {
     const commonStyles = "text-[white] text-2xl uppercase list-none md:text-black md:text-[16px] md:hover:underline"
     return (
         <>
-            <Link to="/" onClick={CloseNav}><li className={commonStyles}>our company</li></Link>
-            <Link to="/" onClick={CloseNav}><li className={commonStyles}>locations</li></Link>
-            <Link to="/" onClick={CloseNav}><li className={commonStyles}>contact</li></Link>
+            <Link to="/company" onClick={() => setOpen(false)}><li className={commonStyles}>our company</li></Link>
+            <Link to="/locations" onClick={() => setOpen(false)}><li className={commonStyles}>locations</li></Link>
+            <Link to="/" onClick={() => setOpen(false)}><li className={commonStyles}>contact</li></Link>
         </>
     );
 };
 
-const MobileMenu = () => (
+const MobileMenu = ({ setOpen }: { setOpen: Function }) => (
     <div className="absolute top-[96px] left-0 w-full h-[235px] bg-black px-[48px] py-[48px] z-[100]
         md:hidden
         ">
         <ul className="flex flex-col w-full h-full justify-between">
-            <Links open />
+            <Links setOpen={setOpen} />
         </ul>
         {/* overlay */}
         <div className="absolute top-[235px] left-0 w-full h-screen bg-black opacity-60" />
@@ -69,11 +63,11 @@ const Nav: React.FC = () => {
             <button onClick={() => setOpen(!open)} className="md:hidden">
                 {open ? <AiOutlineClose fontSize="30" /> : <GiHamburgerMenu fontSize="30" />}
             </button>
-            {open && (<MobileMenu />)}
+            {open && (<MobileMenu setOpen={setOpen} />)}
 
             {/* tablet & desktop nav */}
             <DesktopMenu>
-                <Links open />
+                <Links setOpen={setOpen} />
             </DesktopMenu>
         </Container>
     );
