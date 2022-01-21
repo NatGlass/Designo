0590a405dee84d93cf7ff50859fe917bcf31714c
Nathan G <Nathanglass26@gmail.com>
2022-01-21 13:54:15 +0000

    include scroll top hook

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import ScrollTop from './ScrollTop'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
