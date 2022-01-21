6fac6c83fe0692adf67a10f06ce9d9734efdb8ec
Nathan G <Nathanglass26@gmail.com>
2022-01-21 12:45:00 +0000

    close mobile nav on link click

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,7 +9,7 @@ import { AiOutlineClose } from 'react-icons/ai';
 
 const Container = ({ children }: { children: React.ReactNode }) => (
     <nav className="
-        flex w-full z-10 justify-between items-center
+        flex w-full z-[100] justify-between items-center
         h-[96px] px-[24px]
         md:h-[27px] md:px-[40px] md:my-[64px]
         lg:px-[15px]
@@ -25,26 +25,29 @@ const Logo = () => (
     </Link>
 );
 
-const Links = () => {
+const Links = ({ open }: { open: boolean }, { setOpen }: { setOpen: Function }) => {
+    function CloseNav() {
+        setOpen(!open)
+    }
     const commonStyles = "text-[white] text-2xl uppercase list-none md:text-black md:text-[16px] md:hover:underline"
     return (
         <>
-            <Link to=""><li className={commonStyles}>our company</li></Link>
-            <Link to=""><li className={commonStyles}>locations</li></Link>
-            <Link to=""><li className={commonStyles}>contact</li></Link>
+            <Link to="/" onClick={CloseNav}><li className={commonStyles}>our company</li></Link>
+            <Link to="/" onClick={CloseNav}><li className={commonStyles}>locations</li></Link>
+            <Link to="/" onClick={CloseNav}><li className={commonStyles}>contact</li></Link>
         </>
     );
 };
 
 const MobileMenu = () => (
-    <div className="absolute top-[96px] left-0 w-full h-[235px] bg-black px-[48px] py-[48px]
+    <div className="absolute top-[96px] left-0 w-full h-[235px] bg-black px-[48px] py-[48px] z-[100]
         md:hidden
         ">
         <ul className="flex flex-col w-full h-full justify-between">
-            <Links />
+            <Links open />
         </ul>
         {/* overlay */}
-        <div className="fixed top-[331px] left-0 w-full h-screen bg-black opacity-60 " />
+        <div className="absolute top-[235px] left-0 w-full h-screen bg-black opacity-60" />
     </div>
 );
 
@@ -70,7 +73,7 @@ const Nav: React.FC = () => {
 
             {/* tablet & desktop nav */}
             <DesktopMenu>
-                <Links />
+                <Links open />
             </DesktopMenu>
         </Container>
     );
