ca924fa1e3c9172c1f4c42a7e05f243d2532f0d2
Nathan G <Nathanglass26@gmail.com>
2022-01-22 15:19:32 +0000

    company page hero and section 1

--- src/pages/Company.tsx
+++ src/pages/Company.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import * as React from 'react';
+import HeroSm from '../assets/about/mobile/image-about-hero.jpg'
+import HeroMd from '../assets/about/tablet/image-about-hero.jpg'
+import HeroLg from '../assets/about/desktop/image-about-hero.jpg'
 
-const Company = () => {
-  return <div>company</div>;
-};
+import WomanSm from '../assets/about/mobile/image-world-class-talent.jpg'
+import WomanMd from '../assets/about/tablet/image-world-class-talent.jpg'
+import WomanLg from '../assets/about/desktop/image-world-class-talent.jpg'
+
+const Hero = () => (
+  <div className="flex flex-col w-[full] h-[715px] mx-auto md:w-[90%] md:mb-[120px] lg:flex-row-reverse lg:w-[95%] lg:h-[480px] lg:mb-[160px] max:w-full">
+    <picture className="w-full">
+      <source media="(min-width:976px)" srcSet={HeroLg} />
+      <source media="(min-width:500px)" srcSet={HeroMd} />
+      <img src={HeroSm} className="w-full max-h-[320px] object-cover md:rounded-t-[15px] lg:max-h-[480px] lg:rounded-r-[15px] lg:rounded-l-none" />
+    </picture>
+    <div className="w-full h-full bg-peach px-[20px] py-[60px] md:px-[40px] md:rounded-b-[15px] lg:rounded-l-[15px] lg:rounded-br-none lg:px-[30px] lg:py-[75px]">
+      <h1 className="text-[white] text-center text-[32px] md:text-[48px] lg:text-left ">About Us</h1>
+      <p className="text-[white] text-center leading-[25px] mt-[20px] lg:text-left">
+        Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences.
+      </p>
+    </div>
+  </div>
+);
+
+const Company = () => (
+  <>
+    <Hero />
+    <div className="flex flex-col w-full h-[865px] mb-[60px] mx-auto md:w-[90%] md:h-[736px] md:mb-[120px] lg:flex-row lg:w-[95%] lg:h-[640px] lg:mb-[160px] max:w-full">
+      <picture className="w-full">
+        <source media="(min-width:976px)" srcSet={WomanLg} />
+        <source media="(min-width:500px)" srcSet={WomanMd} />
+        <img src={WomanSm} className="w-full max-h-[320px] md:rounded-t-[15px] lg:max-h-[640px] lg:rounded-tr-none lg:rounded-bl-[15px]" />
+      </picture>
+      <div className="w-full h-full bg-[#FDF3F0] px-[20px] py-[60px] md:px-[40px] md:rounded-[15px] lg:rounded-l-none lg:px-[30px] lg:py-[75px]">
+        <h2 className="text-peach text-center text-[32px] md:text-[40px] lg:text-left">World class talent</h2>
+        <p className="text-peach text-center leading-[25px] mt-[20px] lg:text-left">
+          We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.
+          <br /><br />
+          Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.
+      </p>
+      </div>
+    </div>
+  </>
+);
 
 export default Company;
